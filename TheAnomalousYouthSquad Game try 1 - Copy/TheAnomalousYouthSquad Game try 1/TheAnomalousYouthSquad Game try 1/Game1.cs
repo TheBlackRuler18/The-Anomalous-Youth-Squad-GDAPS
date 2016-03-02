42d747cdce0f47d248be4cfd12d797ae2b848bfc
Herman McElveen <hxm1417@g.rit.edu>
2016-03-02 10:15:29 -0800

    a

---
+++ TheAnomalousYouthSquad Game try 1 - Copy/TheAnomalousYouthSquad Game try 1/TheAnomalousYouthSquad Game try 1/Game1.cs
@@ -0,0 +1,290 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+
+namespace TheAnomalousYouthSquad_Game_try_1
+{
+    // Staes for the game
+    enum GameStates { Intro, TitleScreen, Options, Credits}
+
+
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        // Set up State variable
+        GameStates gState;
+
+        // Picture Properties for intro screen
+        Texture2D bCircle;
+        Texture2D gCircle;
+        Texture2D pCircle;
+        Texture2D wCircle;
+        Vector2 BluePosition;
+        Vector2 GreenPosition;
+        Vector2 PurplePosition;
+        Vector2 WhitePosition;
+
+        // Font Properties
+        SpriteFont font;
+
+        // Background Picture
+        Texture2D GameBackground;
+        
+
+        // Sound Effects for game
+        SoundEffect TitleScreenMusic;
+
+
+        // Variables for collision in intro screen
+        Point BlueFrameSize = new Point(310, 310);
+        Point GreenFrameSize = new Point(310, 310);
+        Point PurpleFrameSize = new Point(310, 310);
+
+        int BlueCollisionOffSet = 10;
+        int GreenCollisionOffSet = 10;
+        int PurpleCollisionOffSet = 10;
+
+        // Collision Methos
+        protected bool Collide()
+        {
+            // Drawing a rectangle around Blue Circle
+            Rectangle BlueRect = new Rectangle(
+                (int)BluePosition.X + BlueCollisionOffSet,
+                (int)BluePosition.Y + BlueCollisionOffSet,
+                BlueFrameSize.X - (BlueCollisionOffSet * 2),
+                BlueFrameSize.Y - (BlueCollisionOffSet * 2));
+
+            // Drawing a rectangle around the Green circle
+            Rectangle GreenRect = new Rectangle(
+                (int)GreenPosition.X + GreenCollisionOffSet,
+                (int)GreenPosition.Y + GreenCollisionOffSet,
+                GreenFrameSize.X - (GreenCollisionOffSet * 2),
+                GreenFrameSize.Y - (GreenCollisionOffSet * 2));
+
+            // Drawing a rectangle around the Purple circle
+            Rectangle PurpleRect = new Rectangle(
+                (int)PurplePosition.X + PurpleCollisionOffSet,
+                (int)PurplePosition.Y + PurpleCollisionOffSet,
+                PurpleFrameSize.X - (PurpleCollisionOffSet * 2),
+                PurpleFrameSize.Y - (PurpleCollisionOffSet * 2));
+
+            return BlueRect.Intersects(GreenRect);
+        }
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            // Makes the screen Bigger
+            graphics.PreferredBackBufferWidth = 1520;
+            graphics.PreferredBackBufferHeight = 850;
+            graphics.ApplyChanges();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            // Assigning values to the vector2 
+            BluePosition = new Vector2(0, 395);
+            GreenPosition = new Vector2(1420, 395);
+            PurplePosition = new Vector2(710, 0);
+            WhitePosition = new Vector2(710, 395);
+
+            base.Initialize();
+
+            // Make mouse curson appear on screen
+            this.IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            // Load in circles for intro
+            bCircle = Content.Load<Texture2D>("Test Circle 1(Blue)");
+            gCircle = Content.Load<Texture2D>("Test Circle 4(Green)");
+            pCircle = Content.Load<Texture2D>("Test Circle 1(Pink)");
+            wCircle = Content.Load<Texture2D>("Test Circle 1(White)");
+
+            // Load in font
+            font = Content.Load<SpriteFont>("SpriteFont1");
+
+            // Load in BackGround
+            GameBackground = Content.Load<Texture2D>("Background for GDAPS Game");
+
+            // Load in TitleMusic
+            TitleScreenMusic = Content.Load<SoundEffect>("GameTitle Music");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+
+            // Update for each state
+            switch (gState)
+            {
+                case GameStates.Intro:
+                    UpdateIntro(gameTime);
+                    TitleScreenMusic.Play();
+                    BluePosition.X += 2;
+                    GreenPosition.X -= 2;
+                    PurplePosition.Y += .95f;
+                    this.IsMouseVisible = false;
+                    // Collide If statement for intro
+                    if (Collide())
+                    {
+                        System.Threading.Thread.Sleep(5000);
+                        gState = GameStates.TitleScreen;
+                    }
+                    break;
+
+                case GameStates.TitleScreen:
+                    UpdateTitleScreen(gameTime);
+                    this.IsMouseVisible = false;
+                    break;
+
+                case GameStates.Options:
+                    UpdateOptions(gameTime);
+                    this.IsMouseVisible = true;
+                    break;
+
+                case GameStates.Credits:
+                    UpdateCredits(gameTime);
+                    this.IsMouseVisible = true;
+                    break;
+            }
+
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+
+            // Draw for each state
+            switch (gState)
+            {
+                case GameStates.Intro:
+                    DrawIntro(gameTime);
+                    break;
+
+                case GameStates.TitleScreen:
+                    DrawTitleScreen(gameTime);
+                    break;
+
+                case GameStates.Options:
+                    DrawOptions(gameTime);
+                    break;
+
+                case GameStates.Credits:
+                    DrawCredits(gameTime);
+                    break;
+            }
+
+        }
+
+        // Update Methos for each state
+        protected void UpdateIntro(GameTime gameTime)
+        {
+            
+        }
+
+        protected void UpdateTitleScreen(GameTime gameTime)
+        {
+           
+        }
+
+        protected void UpdateOptions(GameTime gameTime)
+        {
+
+        }
+
+        protected void UpdateCredits(GameTime gameTime)
+        {
+
+        }
+
+        // Draw Methods for each state
+        protected void DrawIntro(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(bCircle, BluePosition, Color.White);
+            spriteBatch.Draw(gCircle, GreenPosition, Color.White);
+            spriteBatch.Draw(pCircle, PurplePosition, Color.White);
+            // I statement to draw white circle to the screen
+            spriteBatch.Draw(wCircle, WhitePosition, Color.White);
+            
+            spriteBatch.End();
+        }
+
+        protected void DrawTitleScreen(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            // Draws Background to the screen
+            spriteBatch.Draw(GameBackground, new Rectangle(0,0, GraphicsDevice.Viewport.Width,GraphicsDevice.Viewport.Height), Color.White);
+            spriteBatch.End();
+        }
+
+        protected void DrawOptions(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.End();
+        }
+
+        protected void DrawCredits(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.End();
+        }
+
+    }
+}
---
+++ TheAnomalousYouthSquad Game try 1 - Copy/TheAnomalousYouthSquad Game try 1/TheAnomalousYouthSquad Game try 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TheAnomalousYouthSquad Game try 1")]
+[assembly: AssemblyProduct("TheAnomalousYouthSquad Game try 1")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6712fb00-7ab0-4f84-a733-1b40af5c3318")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.4.0.456")]
+[assembly: AssemblyFileVersion("3.4.0.456")]
